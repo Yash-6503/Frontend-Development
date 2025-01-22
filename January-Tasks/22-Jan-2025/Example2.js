972e14414b127d14d0139ccab65cdb3cf35ce6ac
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 19:46:26 +0530

    Program to perform mathematical operations and display it on console.

---
+++ January-Tasks/22-Jan-2025/Example2.js
@@ -0,0 +1,21 @@
+/*
+Write a 'JavaScript' program to declare and initialize two variable like let a=20,b=10 
+and print the Addition, subtraction, multiplication, division, Modulation operation 
+and print the output on console.
+*/
+
+let a = 20, b = 10;
+
+console.log("Addition : ", a + b);
+console.log("Substraction : ", a - b);
+console.log("Multiplication : ", a * b);
+console.log("Division : ", a / b);
+console.log("Modulation : ", a % b);
+
+/* output
+Addition :  30
+Substraction :  10
+Multiplication :  200
+Division :  2
+Modulation :  0
+*/
