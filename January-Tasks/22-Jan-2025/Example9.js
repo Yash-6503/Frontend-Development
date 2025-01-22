43f9f5a9786f6ac0c9574f8a57e4918c7822306e
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 22:44:35 +0530

    Write a 'JavaScript' program to find previous multiple of 10 of given 2 digit value only.

---
+++ January-Tasks/22-Jan-2025/Example9.js
@@ -0,0 +1,11 @@
+/*
+Write a 'JavaScript' program to find previous multiple of 10 of given 2 digit value only.
+*/
+
+let num = 26;
+let result = num - num % 10;
+console.log(`Previous multiple of 10 of ${num} is: ${result}`);
+
+/*
+Previous multiple of 10 of 26 is: 20
+*/
