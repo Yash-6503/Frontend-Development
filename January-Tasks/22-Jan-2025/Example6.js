ba51e951707c840af1678b8e568b3b90a1db93b6
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 22:17:48 +0530

    Write a JavaScript program to calculate and print the area of a square and rectangle and print the output on the console.

---
+++ January-Tasks/22-Jan-2025/Example6.js
@@ -0,0 +1,21 @@
+/*
+Write a JavaScript program to calculate and print the area of a square and rectangle 
+and print the output on the console.
+*/
+
+let side = 5;
+
+let length = 4;
+let breadth = 5;
+
+let areaOfSquare = side * side;
+
+let areaOfRectangle = length * breadth;
+
+console.log(`The area of the square with side ${side} is: ${areaOfSquare}`);
+console.log(`The area of the rectangle with length ${length} and breadth ${breadth} is: ${areaOfRectangle}`);
+
+/* output
+The area of the square with side 5 is: 25
+The area of the rectangle with length 4 and breadth 5 is: 20
+*/
