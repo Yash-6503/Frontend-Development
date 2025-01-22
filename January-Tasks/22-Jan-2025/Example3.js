93a723befd0032bf94bf23714d5606713eb27ade
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 22:30:01 +0530

    Write a 'JavaScript' program to print Addition of any two positive numbers without using (+) operator and print the output on console.

---
+++ January-Tasks/22-Jan-2025/Example3.js
@@ -0,0 +1,15 @@
+/*
+Write a 'JavaScript' program to print Addition of any two positive 
+numbers without using (+) operator and print the output on console.
+*/
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(`Addition of ${num1} and ${num2} is: ${num1 - -num2}`);
+// console.log(`Addition of ${num1} and ${num2} is: ${num1 - ~num2 - 1}`);
+
+
+/* output
+Addition of 10 and 20 is: 30
+*/
