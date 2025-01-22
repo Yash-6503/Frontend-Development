f3b9d5434d9a21485a799a6ce17f54256b23afcb
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 22:46:34 +0530

    Write a 'JavaScript' program to find next multiple of 10 of given 2 digit value only.

---
+++ January-Tasks/22-Jan-2025/Example10.js
@@ -0,0 +1,11 @@
+/*
+Write a 'JavaScript' program to find next multiple of 10 of given 2 digit value only.
+*/
+
+let num = 34;
+let result = parseInt(num / 10 + 1) * 10;
+console.log(`Next multiple of 10 of ${num} is: ${result}`);
+
+/*
+Next multiple of 10 of 34 is: 40
+*/
