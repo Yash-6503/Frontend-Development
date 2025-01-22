9add3dc869ecdd6a1b3881bb152ab4b9aa1b08cf
Yash-6503 <yashwalke6503@gmail.com>
2025-01-22 19:37:44 +0530

    Write a 'JavaScript' program to print student details using console.log() function only.

---
+++ January-Tasks/22-Jan-2025/Example1.js
@@ -0,0 +1,16 @@
+// Write a 'JavaScript' program to print student details using console.log() function only.
+
+console.log("Name = Rohit Sharma");
+console.log("Roll No = 1001");
+console.log("Branch = B. Tech");
+console.log("Address = Hyderabad");
+console.log("Collage Name = Naresh It");
+
+
+/* output
+Name = Rohit Sharma
+Roll No = 1001
+Branch = B. Tech
+Address = Hyderabad
+Collage Name = Naresh It
+*/
