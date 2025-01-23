c88f1d6e0b5cd6eaea9c2701052f0724848899f5
Yash-6503 <yashwalke6503@gmail.com>
2025-01-23 23:30:16 +0530

    Write a 'JavaScript' program to check and print that the given number is even or odd without using ternary operator and control statements and print the output on the console.

---
+++ January-Tasks/23-Jan-2025/Example1.js
@@ -0,0 +1,18 @@
+/*
+Write a 'JavaScript' program to check and print that the given number is even or odd without using
+ternary operator and control statements and print the output on the console.
+*/
+let num = 125;
+
+num % 2 == 0 && console.log("Even");
+num % 2 != 0 && console.log("Odd");
+
+/* num = 125
+Output:
+Odd
+*/
+
+/* num = 10
+Output:
+Even
+*/
