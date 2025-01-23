4b465c7682d256742828a9bf3a841240bfe01cc6
Yash-6503 <yashwalke6503@gmail.com>
2025-01-24 00:17:22 +0530

    Write a JavaScript program that takes an integer as input and checks if it is a positive, negative, or zero using Ternary Operator.

---
+++ January-Tasks/23-Jan-2025/Example9.js
@@ -0,0 +1,20 @@
+/*
+Write a JavaScript program that takes an integer as input and checks if it is a positive, negative, or zero. 
+Display the appropriate message as output using the ternary operator.  
+*/
+
+let number = 0;
+
+number == 0 ? console.log("you entered zero") : number > 0 ? console.log("you entered is positive number") : console.log("you entered negative number");
+
+/*
+you entered negative number
+*/
+
+/*
+you entered is positive number
+*/
+
+/*
+you entered zero
+*/
