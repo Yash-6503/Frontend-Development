4f15b6841fb178617e5b31f26bf6f45a1c75a5ce
Yash-6503 <yashwalke6503@gmail.com>
2025-01-23 23:38:31 +0530

    Write a 'JavaScript' program to check and print that given number is even or odd using ternary operator.

---
+++ January-Tasks/23-Jan-2025/Example5.js
@@ -0,0 +1,10 @@
+/*
+Write a 'JavaScript' program to check and print that given number is 
+even or odd using ternary operator.
+*/
+
+
+let num = 125;
+
+num % 2 == 0 ? console.log("Even") : console.log("Odd");
+
