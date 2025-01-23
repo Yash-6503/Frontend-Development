fee6ba57c9302ff5ad0150651f7e405b82445aa3
Yash-6503 <yashwalke6503@gmail.com>
2025-01-23 23:33:55 +0530

    Write a 'JavaScript' program to check and print the Maximum number among two numbers without using ternary operator, control statements and predefined methods.

---
+++ January-Tasks/23-Jan-2025/Example2.js
@@ -0,0 +1,15 @@
+/*
+Write a 'JavaScript' program to check and print the Maximum number among two 
+numbers without using ternary operator, control statements and predefined methods.
+*/
+
+let a = 10;
+let b = 20;
+
+a > b && console.log("Max = " + a);
+b > a && console.log("Max = " + b);
+a == b && console.log("Equal");
+
+/*
+Max = 20
+*/
