07a884d849f372675f291c6ca086602def92685c
Yash-6503 <yashwalke6503@gmail.com>
2025-01-24 16:38:19 +0530

    Questions to solve

---
+++ January-Tasks/24-Jan-2025/Example1.js
@@ -0,0 +1,11 @@
+/*
+Write a 'JavaScript' program to read age of person and check the person is eligible for vote or not using if-else.
+*/
+
+
+function isEligible() {
+    let a = age.value;
+
+
+    a >= 18 ? ans.value = "You are eligible to vote." : ans.value = "You are not eligible to vote.";
+}
